2aca4070272d2f96c2c659b851d1c26f1de0dc5d
irtizaaah-school <89998973+irtizaaah-school@users.noreply.github.com>
2022-04-09 22:11:47 -0700

    Implemented remaining methods

--- src/SystemAdmin.java
+++ src/SystemAdmin.java
@@ -1,42 +1,53 @@
 import java.util.Vector;
 
-public class SystemAdmin extends Employee{
-
+public class SystemAdmin extends Employee
+{
     //CONSTRUCTOR
-    public SystemAdmin(String username, String password, AllLogs allLogs){
+    public SystemAdmin(String username, String password)
+    {
         super(username, password);
+        this.setIsLoggedIn(false);
         super.setIsSystemAdmin(true);
     }
 
     //MEMBER FUNCTIONS
     //CREATE
-    public void createSystemAdmin(String username, String password, AllLogs allLogs){
-        Employee newEmployee = new Employee(username, password);
-        allLogs.getAllEmployees().add(newEmployee);
+    public void createSystemAdmin(String username, String password, AllLogs allLogs)
+    {
+        SystemAdmin newAdmin = new SystemAdmin(username, password);
+        allLogs.getAllSystemAdmins().add(newAdmin);
     }
-    public void createEmployee(String username, String password, AllLogs allLogs){
+    
+    public void createEmployee(String username, String password, AllLogs allLogs)
+    {
         Employee newEmployee = new Employee(username, password);
         allLogs.getAllEmployees().add(newEmployee);
     }
 
     //DELETE
-    public void deleteEmployee(String username, AllLogs allLogs){
-        
+    public void deleteEmployee(String username, AllLogs allLogs)
+    {
+        allLogs.removeEmployee(username);
     }
-    public void deleteSystemAdmin(String username, AllLogs allLogs){
-
+    
+    public void deleteSystemAdmin(String username, AllLogs allLogs)
+    {
+    	allLogs.removeSystemAdmin(username);
     }
 
     //GETTERS
-    public Vector<SystemAdmin> getAllSystemAdmins(AllLogs allLogs){
+    public Vector<SystemAdmin> getAllSystemAdmins(AllLogs allLogs)
+    {
         return allLogs.getAllSystemAdmins();
     } 
 
-    public Vector<Employee> getAllEmployees(AllLogs allLogs){
+    public Vector<Employee> getAllEmployees(AllLogs allLogs)
+    {
         return allLogs.getAllEmployees();
     } 
 
-    public Vector<ChatLog> getAllChatLogs(AllLogs allLogs){
+    public Vector<ChatLog> getAllChatLogs(AllLogs allLogs)
+    {
         return allLogs.getAllChatLogs();
     } 
 
