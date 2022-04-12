1e6a2155df0df6c564ec2917c6cf7f9f1cbaa980
asbrar <89217772+asbrar@users.noreply.github.com>
2022-04-12 16:04:41 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,85 +1,130 @@
+
 import java.util.Vector;
 
 public class Employee{
+	
+	String username;
+	String password;
+	boolean isLoggedIn;
+	boolean isSystemAdmin;
+	Vector<ChatLog> allUserChatLogs;
+	
+	public Employee(String username, String password) {
+		this.username = username;
+		this.password = password;
+		isLoggedIn = false;
+		//isSystemAdmin = false;
+		
+	}
+	
+	
+	//Setters
+	public void setUsername(String username) {
+		
+		this.username = username;
+		
+	}
+	
+	public void setPassword(String password) {
+		
+		this.password = password;
+		
+	}
+	
+	public void setIsLoggedIn(boolean isloggedin) {
+		
+		this.isLoggedIn = isloggedin;
+		
+	}
+	
+	public void setAllUserChatLogs(Vector<ChatLog> alluserchatlogs) {
+		
+		this.allUserChatLogs = alluserchatlogs;
+		
+	}
 
-        //MEMBER VARIABLES
-        private String username;
-        private String password;
-        private boolean isLoggedIn;
-        private boolean isSystemAdmin;
-        private Vector<ChatLog> allUserChatLogs;
-
-
-        //CONSTRUCTOR
-        public Employee(String username, String password){
-                this.username = username;
-                this.password = password;
-                this.setIsLoggedIn(false);
-                this.setIsSystemAdmin(false);
-        }
-
-        //SETTERS
-        public void setUsername(String username){
-                this.username = username;
-        }
-
-        public void setPassword(String password){
-                this.password = password;
-        }
-
-        public void setIsLoggedIn(boolean isLoggedIn){
-                this.isLoggedIn = isLoggedIn;
-        }
-
-        public void setIsSystemAdmin(boolean isSystemAdmin){
-                this.isSystemAdmin = isSystemAdmin;
-        }
-
-        public void setAllUserChatLogs(Vector<ChatLog> allUserChatLogs){
-                this.allUserChatLogs = allUserChatLogs;
-        }
-
-
-        //GETTERS
-        public String getUsername(){
-                return this.username;
-        }
-
-        public String getPassword(){
-                return this.password;
-        }
-
-        public boolean getIsLoggedIn(){
-                return this.isLoggedIn;
-        }
-
-        public boolean getIsSystemAdmin(){
-                return this.isSystemAdmin;
-        }
-
-        public Vector<ChatLog> getAllUserChatLogs(){
-                return this.allUserChatLogs;
-        }
-
-        // MEMBER FUNCTIONS
-        public boolean verifyEmployee(String username, String password){
-                return true;
-        }
-
-        public void createUserChatLog(String chatLogName, Vector<String> allMembers){
-                allUserChatLogs.add(new ChatLog(chatLogName, allMembers));
-        }
-
-        public void deleteUserChatLog(String chatLogName){
-
-        }
-
-        public void getUserChatLog(String chatLogName){
-
-        }
-
-        public void sendMessage(String chatLogName, Message message){
-
-        }
-
+	
+	
+	
+	//getters
+	public String getUsername() {
+		
+		return this.username;
+		
+	}
+	
+	public String getPassword() {
+		
+		return this.password;
+		
+	}
+	
+	public boolean getIsLoggedIn() {
+		
+		return this.isLoggedIn;
+		
+	}
+	
+	public Vector<ChatLog> getAllUserChatLogs() {
+		
+		return this.allUserChatLogs;
+		
+	}
+	
+	public boolean getIsSystemAdmin() {
+		
+		return this.isSystemAdmin;
+		
+	}
+	
+	
+	
+	
+	public boolean verifyEmployee(String username, String password) {
+		if(this.username.equals(username) && this.password.equals(password)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+	public void createUserChatLog(String chatLogName, Vector<String> allMembers) {
+		allUserChatLogs.add(new ChatLog(chatLogName, allMembers));
+		
+	}
+	
+	public void deleteUserChatLog(String ChatLogName){
+		for(int i = 0; i < allUserChatLogs.size(); i++) {
+			if(allUserChatLogs.get(i).getChatLogName().equals(ChatLogName)) {
+				allUserChatLogs.remove(i);
+			}
+		}
+	}
+	
+	public ChatLog getUserChatLog(String ChatLogName){
+		int index = 0;
+		for(int i = 0; i < allUserChatLogs.size(); i++) {
+			if(allUserChatLogs.get(i).getChatLogName().equals(ChatLogName)) {
+				index = i;
+			}
+		}
+		return allUserChatLogs.get(index);
+	}
+	
+	public void sendMessage(String ChatLogName, Message message) {
+		for(int i = 0; i < allUserChatLogs.size(); i++) {
+			if(allUserChatLogs.get(i).getChatLogName().equals(ChatLogName)) {
+				allUserChatLogs.get(i).addMessage(message);
+			}
+		}
+		
+		//send message part still pending.
+		
+	}
+	
+	
+	
+	
 }
