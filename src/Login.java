b3de798bc293cb1292c58af11d91002f1ccb3c34
marcosbarajas <98499992+marcosbarajas@users.noreply.github.com>
2022-04-24 15:11:12 -0700

    Add files via upload
    basic GUI - w/o text file implementation for now

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Login{
+	public static void main(String[] a){
+		Frame frame = new Frame();
+		frame.setTitle("Messenger login");
+		frame.setVisible(true);
+		frame.setBounds(10, 10, 230, 180);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		}
+	}
